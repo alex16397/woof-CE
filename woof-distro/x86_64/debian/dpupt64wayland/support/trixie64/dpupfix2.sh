c92db0eb63151bc516815c4b567ee0f4048c87a1
alex16397 <103257940+alex16397@users.noreply.github.com>
2026-01-26 15:56:45 +0300

    default browser

--- woof-distro/x86_64/debian/dpupt64wayland/support/trixie64/dpupfix2.sh
+++ woof-distro/x86_64/debian/dpupt64wayland/support/trixie64/dpupfix2.sh
@@ -7,7 +7,6 @@ sed -i 's%</keyboard>%<keybind key="A-z"><action name="Execute"><command>/usr/bi
 
 # temp
 ( cd root/Desktop
-sed -i 's/^Exec.*$/Exec=/usr/bin/chromium/' browse.desktop
 mv browse.desktop browse_app.desktop
 mv file.desktop file_app.desktop
 mv settings.desktop setup_app.desktop
@@ -23,6 +22,9 @@ echo '1=file_app.desktop
 5=trash_app.desktop' > root/.config/zzzfm/desktop0
 
 echo 'minimalapps' > root/.config/zzzfm/applayout
+
+cd /usr/local/bin
+sed -i 's/^exec.*$/exec /usr/bin/chromium/' defaultbrowser
 
 # end temp
 
